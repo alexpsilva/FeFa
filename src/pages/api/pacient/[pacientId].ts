8dea6503783613c9848ad9f33e5533cdb266b516
alexpsilva <alex.paivasilva@gmail.com>
2025-04-05 09:16:36 -0300

    Added update pacient

--- src/pages/api/pacient/[pacientId].ts
+++ src/pages/api/pacient/[pacientId].ts
@@ -1,8 +1,9 @@
 import type { APIRoute } from "astro";
 
-import { DeletePacientSchema } from "@core/pacient/types";
-import { pacientRepository } from "src";
+import { DeletePacientSchema, UpdatePacientSchema } from "@core/pacient/types";
+import formDataToJson from "@core/shared/utils/formDataToJson";
 import tryCatch from "@core/shared/utils/tryCatch";
+import { pacientRepository } from "src";
 
 export const DELETE: APIRoute = async ({ params, locals }) => {
     const parseResult = DeletePacientSchema.safeParse({id: params.pacientId, userId: locals.userId});
@@ -17,4 +18,19 @@ export const DELETE: APIRoute = async ({ params, locals }) => {
     }
 
     return new Response(null, { status: 204, headers: { 'X-Redirect-To': `/pacient` } });
+}
+
+export const PUT: APIRoute = async ({ params, request, locals }) => {
+    const formData = formDataToJson(await request.formData());
+    const parseResult = UpdatePacientSchema.safeParse({...formData, id: params.pacientId, userId: locals.userId});
+    if (!parseResult.success) {
+        return new Response(JSON.stringify({ error: parseResult.error }), { status: 400 });
+    }
+
+    const repositoryResult = await tryCatch(pacientRepository.update(parseResult.data));
+    if (!repositoryResult.success) {
+        return new Response(JSON.stringify({ error: repositoryResult.error }), { status: 500 });
+    }
+
+    return new Response(null, { status: 204, headers: { 'X-Redirect-To': `/pacient/${params.pacientId}` } });
 }
